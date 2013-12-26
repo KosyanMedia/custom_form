0ae9993f3ac54819f70ac2954773b5149f3c34ba
Ainur <nur.php@gmail.com>
2013-12-26 12:45:44 +0700

    Revert "rm file cache"
    This reverts commit cf19e8797910a9d9354f779face47c7082cf78ad.

--- widget.php
+++ widget.php
@@ -2,6 +2,12 @@
 header('Content-Type: text/javascript;charset=UTF-8');
 header('Cache-Control: max-age=3600');
 $widget_id = basename($_GET['id']);
-$widget_url = 'http://www.travelpayouts.com/widgets/' . $widget_id .'.js';
-$data = file_get_contents($widget_url);
+$cached_file = './cache/' . $widget_id;
+if (is_file($cached_file)){
+    $data = file_get_contents($cached_file);
+} else {
+    $widget_url = 'http://www.travelpayouts.com/widgets/' . $widget_id .'.js';
+    $data = file_get_contents($widget_url);
+    file_put_contents($cached_file, $data);
+}
 echo $data;
